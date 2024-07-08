79349197b3cd03b0443c59e3c7b7158e51403470
Berenger Bramas <Berenger.Bramas@inria.fr>
2024-07-08 14:11:19 +0200

    update

---
+++ SDK/optixMultiTriangle/optixLJ.cu
@@ -0,0 +1,327 @@
+//
+// Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include <optix.h>
+
+#include "optixTriangle.h"
+#include <cuda/helpers.h>
+
+#include <sutil/vec_math.h>
+
+
+// OLD
+/*
+extern "C" {
+__constant__ Params params;
+}
+
+
+static __forceinline__ __device__ void setPayload( float3 p )
+{
+    optixSetPayload_0( __float_as_uint( p.x ) );
+    optixSetPayload_1( __float_as_uint( p.y ) );
+    optixSetPayload_2( __float_as_uint( p.z ) );
+}
+
+
+static __forceinline__ __device__ void computeRay( uint3 idx, uint3 dim, float3& origin, float3& direction )
+{
+    const float3 U = params.cam_u;
+    const float3 V = params.cam_v;
+    const float3 W = params.cam_w;
+    const float2 d = 2.0f * make_float2(
+            static_cast<float>( idx.x ) / static_cast<float>( dim.x ),
+            static_cast<float>( idx.y ) / static_cast<float>( dim.y )
+            ) - 1.0f;
+
+    origin    = params.cam_eye;
+    direction = normalize( d.x * U + d.y * V + W );
+}
+
+
+extern "C" __global__ void __raygen__rg()
+{
+    // Lookup our location within the launch grid
+    const uint3 idx = optixGetLaunchIndex();
+    const uint3 dim = optixGetLaunchDimensions();
+
+    // Map our launch idx to a screen location and create a ray from the camera
+    // location through the screen
+    float3 ray_origin, ray_direction;
+    computeRay( idx, dim, ray_origin, ray_direction );
+
+    // Trace the ray against our scene hierarchy
+    unsigned int p0, p1, p2;
+    optixTrace(
+            params.handle,
+            ray_origin,
+            ray_direction,
+            0.0f,                // Min intersection distance
+            1e16f,               // Max intersection distance
+            0.0f,                // rayTime -- used for motion blur
+            OptixVisibilityMask( 255 ), // Specify always visible
+            OPTIX_RAY_FLAG_NONE,
+            0,                   // SBT offset   -- See SBT discussion
+            1,                   // SBT stride   -- See SBT discussion
+            0,                   // missSBTIndex -- See SBT discussion
+            p0, p1, p2 );
+    float3 result;
+    result.x = __uint_as_float( p0 );
+    result.y = __uint_as_float( p1 );
+    result.z = __uint_as_float( p2 );
+
+    // Record results in our output raster
+    params.image[idx.y * params.image_width + idx.x] = make_color( result );
+}
+
+
+extern "C" __global__ void __miss__ms()
+{
+    MissData* miss_data  = reinterpret_cast<MissData*>( optixGetSbtDataPointer() );
+    setPayload(  miss_data->bg_color );
+}
+
+
+
+extern "C" __global__ void __closesthit__ch()
+{
+    // When built-in triangle intersection is used, a number of fundamental
+    // attributes are provided by the OptiX API, indlucing barycentric coordinates.
+    //const float2 barycentrics = optixGetTriangleBarycentrics();
+    //setPayload( make_float3( barycentrics, 1.0f ) );
+    const unsigned int prim_idx = optixGetPrimitiveIndex();
+    const float3 color = generate_color(prim_idx/4);
+    const float2 barycentrics = optixGetTriangleBarycentrics();
+
+    setPayload( make_float3( barycentrics.x * color.x,
+                             barycentrics.y * color.y,
+                             barycentrics.x * barycentrics.y * color.z) );
+}
+*/
+// NEW
+
+
+extern "C" {
+__constant__ ParamsLJ params;
+}
+
+
+static __forceinline__ __device__ void trace(
+        OptixTraversableHandle handle,
+        float3                 ray_origin,
+        float3                 ray_direction,
+        float                  tmin,
+        float                  tmax,
+        float3                 partPos,
+        float                  c,
+        float*                 energy
+        )
+{
+    unsigned int p0, p1, p2, p3, p4, p5;
+    p0 = __float_as_uint( partPos.x );
+    p1 = __float_as_uint( partPos.y );
+    p2 = __float_as_uint( partPos.z );
+    p3 = __float_as_uint( *energy );
+    p4 = 0;
+    p5 = __float_as_uint( c );
+
+    while(tmin < tmax){
+        optixTrace(
+                handle,
+                ray_origin,
+                ray_direction,
+                tmin,
+                tmax,
+                0.0f,                // rayTime
+                OptixVisibilityMask( 1 ),
+                OPTIX_RAY_FLAG_NONE,
+                0,                   // SBT offset
+                0,                   // SBT stride
+                0,                   // missSBTIndex
+                p0, p1, p2, p3, p4, p5);
+
+        tmin = __uint_as_float( p4 );
+    }
+    
+    (*energy) += __uint_as_float( p3 );
+}
+
+static __forceinline__ __device__ float getPayloadC()
+{
+    return __uint_as_float( optixGetPayload_5() );
+}
+
+static __forceinline__ __device__ void setPayloadTmin( float tmin )
+{
+    optixSetPayload_4( __float_as_uint( tmin ) );
+}
+
+static __forceinline__ __device__ void setPayloadEnergy( float p )
+{
+    optixSetPayload_3( __float_as_uint( p ) );
+}
+
+static __forceinline__ __device__ float getPayloadEnergy()
+{
+    return __uint_as_float( optixGetPayload_3() );
+}
+
+static __forceinline__ __device__ float3 getPayloadPartPos()
+{
+    float3 point;
+    point.x = __uint_as_float( optixGetPayload_0() );
+    point.y = __uint_as_float( optixGetPayload_1() );
+    point.z = __uint_as_float( optixGetPayload_2() );
+    return point;
+}
+
+
+extern "C" __global__ void __raygen__rg()
+{
+    const uint3 idx = optixGetLaunchIndex();
+    // const uint3 dim = optixGetLaunchDimensions();
+    const int point_index = idx.x;
+
+    const RayGenDataLJ* rtData = (RayGenDataLJ*)optixGetSbtDataPointer();
+    float3 point;
+    point.x = params.points[point_index];
+    point.y = params.points[point_index + params.leading_dim];
+    point.z = params.points[point_index + params.leading_dim*2];
+    const float c = params.c;
+    const float half_ray = params.c;
+
+    // const int ray_index = idx.y;
+    // const float3 ray_origins[3] = {
+    //     make_float3(point.x - half_ray, point.y, point.z),
+    //     make_float3(point.x, point.y - half_ray, point.z),
+    //     make_float3(point.x, point.y, point.z - half_ray)
+    // };
+    // const float3 ray_directions[3] = {
+    //     make_float3(2 * half_ray, 0, 0),
+    //     make_float3(0, 2 * half_ray, 0),
+    //     make_float3(0, 0, 2 * half_ray)
+    // };
+    // const float3 origin = ray_origins[ray_index];
+    // const float3 direction = normalize(ray_directions[ray_index]);
+
+    float3 origin;
+    float3 direction;
+    {
+        float xcoef = (idx.y == 0) ? 1.0f : 0.0f;
+        float ycoef = (idx.y == 1) ? 1.0f : 0.0f;
+        float zcoef = (idx.y == 2) ? 1.0f : 0.0f;
+
+        origin = make_float3(point.x - half_ray * xcoef, point.y - half_ray * ycoef, point.z - half_ray * zcoef);
+        direction = make_float3(xcoef, ycoef, zcoef);
+    }
+
+    float payload_energy = 0;
+    trace( params.handle,
+            origin,
+            direction,
+            0.00f,  // tmin
+            2 * half_ray,  // tmax
+            point,
+            c,
+            &payload_energy );
+
+    atomicAdd(&params.energy[point_index], payload_energy);
+}
+
+
+extern "C" __global__ void __miss__ms()
+{
+    const float MAX_FLOAT = 3.402823466e+38F;
+    setPayloadTmin( MAX_FLOAT );
+}
+
+
+// Function to calculate the Lennard-Jones potential between two particles
+static __forceinline__ __device__  float  lennardJonesPotential(const float3 p1, 
+                                                                const float3 p2, 
+                                                                const float dist_squared,
+                                                                const float epsilon, 
+                                                                const float sigma) {
+    const float r = sqrt(dist_squared);//distance(p1, p2);
+    const float sigma_d_r = sigma / r;
+    const float r6 = (sigma_d_r*sigma_d_r)*(sigma_d_r*sigma_d_r)*(sigma_d_r*sigma_d_r);
+    const float r12 = r6 * r6;
+    const float result = float(4) * epsilon * (r12 - r6);
+    return result;
+}
+
+
+extern "C" __global__ void __closesthit__ch()
+{
+    const unsigned int           prim_idx    = optixGetPrimitiveIndex();
+    const OptixTraversableHandle gas         = optixGetGASTraversableHandle();
+    const unsigned int           sbtGASIndex = optixGetSbtGASIndex();
+
+    float4 vertices[3];
+    // sphere center (q.x, q.y, q.z), sphere radius q.w
+    optixGetSphereData( gas, prim_idx, sbtGASIndex, 0.f, vertices );
+
+    float3 q; // TODO
+
+    const float3 point = getPayloadPartPos();
+    const float3 diff_pos{fabsf(point.x - q.x), fabsf(point.y - q.y), fabsf(point.z - q.z)};
+    const float3 diff_pos_squared{diff_pos.x * diff_pos.x, diff_pos.y * diff_pos.y, diff_pos.z * diff_pos.z};
+    const float3 dist_axis_squared{diff_pos_squared.y + diff_pos_squared.z, diff_pos_squared.x + diff_pos_squared.z, diff_pos_squared.x + diff_pos_squared.y};
+    const float dist_squared = diff_pos_squared.x + diff_pos_squared.y + diff_pos_squared.z;
+    const float c = getPayloadC();
+
+    if(dist_squared < c*c){
+        // const float3 ray_orig = optixGetWorldRayOrigin();
+        const float3 ray_dir  = optixGetWorldRayDirection();
+
+        const float closest_axis_dist = fminf(dist_axis_squared.x, fminf(dist_axis_squared.y, dist_axis_squared.z));
+        const bool closest_axis_is_ray_dir = (closest_axis_dist == dist_axis_squared.x && ray_dir.x != 0) ||
+                                            (closest_axis_dist == dist_axis_squared.y && ray_dir.y != 0) ||
+                                            (closest_axis_dist == dist_axis_squared.z && ray_dir.z != 0);
+
+        if(closest_axis_is_ray_dir){
+            const float epsilon = 1.0f;
+            const float sigma = 1.0f;
+            const float energy = lennardJonesPotential(point, q, dist_squared,
+                                                       epsilon, sigma);
+
+            setPayloadEnergy( getPayloadEnergy() + energy );
+        }
+    }
+
+
+
+    // Backface hit not used.
+    // float  t_hit2 = __uint_as_float( optixGetAttribute_0() ); 
+    // float3 world_raypos = ray_orig + t_hit * ray_dir;
+    // float3 obj_raypos   = optixTransformPointFromWorldToObjectSpace( world_raypos );
+    // float3 obj_normal   = ( obj_raypos - make_float3( q ) ) / q.w;
+    // float3 world_normal = normalize( optixTransformNormalFromObjectToWorldSpace( obj_normal ) );
+    const float  t_hit = optixGetRayTmax();
+    setPayloadTmin( t_hit );
+}
