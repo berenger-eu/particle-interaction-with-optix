ae2c33ff5253654dc114bcc3adc6bfab20f344e3
Berenger Bramas <Berenger.Bramas@inria.fr>
2024-07-10 10:14:22 +0200

    debug

--- SDK/optixMultiTriangle/optixLJ.cu
+++ SDK/optixMultiTriangle/optixLJ.cu
@@ -144,7 +144,7 @@ extern "C" __global__ void __raygen__rg()
         origin = make_float3(point.x - half_ray,
                              point.y + half_ray * ycoef,
                              point.z + half_ray * zcoef);
-        direction = make_float3(c, 0, 0);
+        direction = make_float3(half_ray*2, 0, 0);
     }
 
     // print point, origin and direction in one line
